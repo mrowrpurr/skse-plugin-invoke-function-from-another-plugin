9a59257feb049cb38aabee02567b1fa0fc8a807f
Mrowr Purr <mrowr.purr@gmail.com>
2022-11-14 20:24:23 -0800

    lib bin lib

--- include/MySkseLibraryExample.h
+++ include/MySkseLibraryExample.h
@@ -3,6 +3,6 @@
 #include "MySkseLibraryExample/MyDemoSingleton.h"
 
 namespace MySkseLibraryExample {
-    API_INTERFACE int BasicTest();
+    extern "C" API_INTERFACE int BasicTest();
     extern "C" API_INTERFACE MyDemoSingleton& GetDemoSingleton();
 }
--- src/MySkseLibraryExample/MySkseLibraryExample.cpp
+++ src/MySkseLibraryExample/MySkseLibraryExample.cpp
@@ -1,7 +1,7 @@
 #include "MySkseLibraryExample/MyDemoSingletonImpl.h"
 
 namespace MySkseLibraryExample {
-    int BasicTest() { return 69; }
+    extern "C" int BasicTest() { return 69; }
 
     extern "C" MyDemoSingleton& GetDemoSingleton() {
         return MyDemoSingletonImpl::GetSingleton();
