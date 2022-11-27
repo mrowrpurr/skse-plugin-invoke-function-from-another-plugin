16c541bfcad26b976e6ad88ad443f8370bc243a2
Mrowr Purr <mrowr.purr@gmail.com>
2022-11-26 20:10:56 -0800

    2 SKSE Plugins

---
+++ PluginThatCallsOtherPluginsFunction/plugin.cpp
@@ -0,0 +1,21 @@
+#include <spdlog/sinks/basic_file_sink.h>
+
+void InitializeLog() {
+	auto path = SKSE::log::log_directory();
+	if (!path) SKSE::stl::report_and_fail("Failed to find standard logging directory"sv);
+	*path /= fmt::format("{}.log", SKSE::PluginDeclaration::GetSingleton()->GetName());
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+	const auto level = spdlog::level::trace;
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+	log->set_level(level);
+	log->flush_on(level);
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+	InitializeLog();
+	SKSE::log::info("Hello from Plugin that calls a Function in a different Plugin");
+	SKSE::Init(skse);
+	return true;
+}
